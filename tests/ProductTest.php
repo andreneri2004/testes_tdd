e60a5401aba60398cdc71e0aef25d730ed303e00
andre.neri <andre.neri@saude.ba.gov.br>
2023-02-18 12:40:37 -0300

    ajuste das mensagens dos testes

---
+++ tests/ProductTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Code;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    Public function testSeONomeDoProdutoESetadoCorretamente(){
+        $product = new Product();
+        $product->setName('Carro');
+        $this->assertEquals('Carro', $product->getName(), 'Valores não são iguais');
+    }
+
+    Public function testSeOValorDoProdutoESetadoCorretamente(){
+        $product = new Product();
+        $product->setPrice(520.00);
+        $this->assertEquals(520.00, $product->getPrice(), 'Valores não são iguais');
+    }
+
+    Public function testSeOSlugDoProdutoESetadoCorretamente(){
+        $product = new Product();
+        $product->setSlug('Carro');
+        $this->assertEquals('Carro', $product->getSlug(), 'Valores não são iguais');
+    }
+
+}
